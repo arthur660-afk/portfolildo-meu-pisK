80b95b3a989726d8293e191c0ab6cc0a790051b3
Arthur Wellington <arthurwellington660@gmail.com>
2025-09-01 08:47:40 -0300

    personalisação

--- scrip/script.js
+++ scrip/script.js
@@ -1,4 +1,4 @@
-const tnome = document.getElementById("gnome");
+const tnome = document.getElementById("aut-name");
 let nome = window.prompt("Qual é o seu nome?");
 tnome.innerHTML = `Seja bem-vindo ao meu site, ${nome}! 😇😊😊😓`;
 
