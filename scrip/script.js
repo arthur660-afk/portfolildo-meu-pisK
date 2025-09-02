86fa3a88bc88217f4f980c7803e13935d028bd04
Arthur Wellington <arthurwellington660@gmail.com>
2025-09-02 11:20:30 -0300

    imerção da personalisação

--- scrip/script.js
+++ scrip/script.js
@@ -1,8 +1,25 @@
-const tnome = document.getElementById("aut-name");
-let nome = "Visitante";
-nome = window.prompt("Qual é o seu nome?");
+const tnome = document.getElementById("aut-name");//pegar o nome do usuario
+let nome = window.prompt("Qual é o seu nome?");
 if  (nome === null || nome === "") {
     nome = "Visitante";
-}
-tnome.innerHTML = `Seja bem-vindo ao meu site, ${nome}! 😇😊😊😓`;
+};
+tnome.innerHTML = `Seja bem-vindo ao meu site, ${nome}! 😇😊😊`;//msg com o nome
 
+const navlateral = document.getElementById("nav-lateral");//id da div da navbar lateral
+
+navlateral.addEventListener("mouseleave", sumir );
+
+function sumir() {//função para sumir a navbar
+    navlateral.style.opacity = "0";
+    navlateral.style.transition = "all 0.5s";
+};
+
+
+const butão = document.getElementById("navblat");//pegar o botao
+butão.addEventListener("click", navlat);//adicionar o evento de click
+
+function navlat() {//função para aparecer a navbar
+    
+    navlateral.style.opacity = "1";
+    navlateral.style.transition = "all 0.5s";
+}
